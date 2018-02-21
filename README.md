2fe5c254fc5d2eea9db3dd71f4d7d06aafd05a2f
Felipe Rêgo <feliperrego@gmail.com>
2018-02-20 22:21:42 -0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # html-import-partials
-Easily build a static html file by including html partials
+
+Easily build a static html file by including html partials.
+
+## Usage
+Install via npm
+
+```
+npm install html-import-partials
+```
+
+or yarn 
+
+```
+yarn add html-import-partials
+```
+
+#### Command line:
+
+```
+$ html-import-partials src/*html -o dist
+``` 
+
+#### In Your File:
+In the html file include a `<include>`.
+This tag will be replaced with the `src` you provide.
+
+```
+<include src='location/of/some/file.html'/>
+```
+
+## Examples
+
+### Basic
+Simply include the files where you want them
+
+File: `./example/src/foo.html`
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Foo</title>
+</head>
+<body>
+    <h1>Welcome to my website</h1>
+
+    <include src='partials/header.html'/>
+</body>
+</html>
+```
+
+File: `./example/src/partials/footer.html`
+
+```html
+<p>Footer here!!!</p>
+```
+
+Command: `$ html-import-partials example/src/*html -o example/dist`
+
+Compiled file: `./example/dist/foo.html`
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Foo</title>
+</head>
+<body>
+    <h1>Welcome to my website</h1>
+    
+    <p>Footer here!!!</p>
+</body>
+</html>
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const pkg = require('./package.json');
+const fs = require('fs');
+const path = require('path');
+const watch = require('node-watch');
+
+const openTag = '<include';
+
+var program = require('commander');
+
+program
+.version(pkg.version, '-v, --version')
+.arguments('<file>')
+.option('-o, --out [value]', 'output directory')
+.option('-w, --watch', 'watch any changes')
+.parse(process.argv);
+
+if (!program.args || !program.args.length){
+    program.help();
+    process.exit(8);
+} else if (!program.out) {
+    console.error("You must inform the output directory. Use '-o' our '--out'.");
+    process.exit(1);
+} else {
+    init(program.args);
+}
+
+if (program.watch) {
+    watch(program.args, { recursive: true }, function(evt, file) {
+        console.log('%s changed.', file);
+        parsePartials(file);
+    });
+}
+
+function init(files) {
+
+    if (!fs.existsSync(program.out)){
+        fs.mkdirSync(program.out);
+    }
+
+    files.map(function(file) {
+
+        if (!fs.existsSync(file)) {
+            console.error("The input file could not be found.");
+            process.exit(1);
+        }
+
+        parsePartials(file);
+    })
+}
+
+function parsePartials(file) {
+    var html = fs.realpathSync(file);
+    var content = fs.readFileSync(html).toString();
+    var filepath = path.dirname(file);
+    var found = 0;
+    
+    while (found !== -1) {
+        if (found === -1) {
+            continue;
+        }
+        found = content.indexOf(openTag, found);
+        var end = content.indexOf('>', found) + 1;
+        var partial = content.slice(found, end).toString();
+        var src = get_attrib(partial, 'src');
+        var partial_contents = '';
+
+        if (src !== null) {
+            if (!path.isAbsolute(src)) {
+                src = path.normalize(filepath + '/' + src);
+            }
+            if (fs.existsSync(src) && src !== file) {
+                partial_contents = fs.readFileSync(src).toString();
+            }
+        }
+        content = content.replace(partial, partial_contents);
+    }
+
+    var out_file = path.join(program.out, path.basename(file));
+    fs.writeFile(out_file, content, function(err) {
+        if(err) console.error(err);
+    }); 
+    
+}
+
+function get_attrib(partial, attrib) {
+    attrib = ' ' + attrib + '=';
+    var attrib_len = attrib.length;
+    var start = partial.indexOf(attrib);
+    if (start === -1) { return null; }
+    start += attrib_len;
+    var quoteType = partial.charAt(start);
+    start ++;
+    return partial.slice(start, partial.indexOf(quoteType, start))
+}
